b080c72945435df7920a777640c0cc5978815ebc
Dhuli2708 <88284070+Dhuli2708@users.noreply.github.com>
2023-09-28 04:10:50 -0400

    Add files via upload

---
+++ macchanger.sh
@@ -0,0 +1,22 @@
+ #! /bin/bash
+
+ clear
+ printf "welcome to the bash scripting \n " ;
+printf "\n ============================"
+printf "\n Enter 0"
+
+ 
+ read  a
+
+
+ if (a==1);
+then
+
+ 
+ sudo macchanger -r eth0
+
+#else
+ echo "Oops ! wrong key pressed, try again"
+ 
+ fi
+ 
